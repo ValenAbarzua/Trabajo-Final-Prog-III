51316d8915c3c8727d7917b50852631010cad991
Valentina Abarzua <112896097+ValenAbarzua@users.noreply.github.com>
2026-05-07 02:42:36 +0000

    borrar force true

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ async function startServer() {
     console.log('✅ Database connection established successfully.');
     
     // En desarrollo, sincronizar modelos
-    await sequelize.sync({ force: true }); //CAMBIAR TEMPORALMENTE
+    await sequelize.sync(); //CAMBIAR TEMPORALMENTE
     console.log('✅ Database synchronized');
     
     
